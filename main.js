702f286e29b0f0075323c33a8c143aa5a38e4837
Jina <jinayoussef@hotmail.co.uk>
2023-01-20 04:08:28 +0000

    if statement for string

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const display = document.querySelector("#display")
 const numberButtons = document.querySelectorAll(".number")
+const operatorButtons = document.querySelectorAll(".operator")
 
-const buttonClickEvent = (event) =>{
+let x = 0
+
+const numberButtonClickEvent = (event) =>{
     display.innerHTML += event.target.value
+    x = display.innerHTML
+    if (x.slice(-1).test(/[^0-9]/$)) {
+        display.innerHTML = ""
+    } 
 }
 
 numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener("click", buttonClickEvent)
+    numberButton.addEventListener("click", numberButtonClickEvent)
 })
 
+const operatorButtonClickEvent = (event) =>{
+    if (event.target.value == "+") {
+        x.concat("+")
+    } /*else if (value == "-")*/
+}
+
+console.log(x)
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", operatorButtonClickEvent)
+})
